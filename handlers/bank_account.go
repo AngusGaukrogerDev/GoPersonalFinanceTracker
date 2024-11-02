28986094b5333170505d0e7bf6407c2b25c86eaf
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-11-02 16:19:01 +0000

    Basic call to Starling API

--- handlers/bank_account.go
+++ handlers/bank_account.go
@@ -2,12 +2,49 @@
 package handlers
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetBankAccountBalance(c *gin.Context) {
-	message := "AccountBalance"
-	c.JSON(http.StatusOK, message)
+	url := os.Getenv("STARLING_URL") + "/api/v2/accounts"
+	// "/api/v2/accounts/{accountUid}/balance"
+
+	// Create the HTTP GET request
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatalf("Failed to create request: %v", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("STARLING_ACCESS_TOKEN"))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to make GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check if the response status is OK
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Error response from server: %s", resp.Status)
+	}
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	// Log the output as the message variable
+	res := string(body)
+	log.Println("Response Message:", res)
+
+	// Return the message as JSON
+	c.JSON(http.StatusOK, res)
 }
